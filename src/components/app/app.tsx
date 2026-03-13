83b4c9a44db7cb6dea5223ccd688d948a1dc4a62
Anar Abilov <a.abilov@mkskom.ru>
2026-03-13 13:40:47 +0200

    feat: Сделать роут главной страницы

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,37 +1,22 @@
-import { useGetIngredientsQuery } from '@/services/ingredient/api';
-import { Preloader } from '@krgaa/react-developer-burger-ui-components';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { AppHeader } from '../app-header/app-header';
-import { BurgerConstructor } from '../burger-constructor/burger-constructor';
-import { BurgerIngredients } from '../burger-ingredients/burger-ingredients';
+import { AppHeader } from '@components/app-header/app-header';
+import { Home } from '@pages/Home';
 
 import styles from './app.module.css';
 
 export const App = (): React.JSX.Element => {
-  const { isLoading: loadingApp, error: errorApp } = useGetIngredientsQuery();
-
-  if (errorApp) return <h2>{'Ошибка'}</h2>;
-
   return (
     <div className={styles.app}>
-      {loadingApp && <Preloader />}
+      <AppHeader />
 
-      {!loadingApp && (
-        <>
-          <AppHeader />
-          <h1 className={`${styles.title} text text_type_main-large mt-10 mb-5 pl-5`}>
-            Соберите бургер
-          </h1>
-          <main className={`${styles.main} pl-5 pr-5`}>
-            <DndProvider backend={HTML5Backend}>
-              <BurgerIngredients />
-              <BurgerConstructor />
-            </DndProvider>
-          </main>
-        </>
-      )}
+      <main className={styles.main}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </main>
     </div>
   );
 };
---
+++ src/pages/Home.tsx
@@ -0,0 +1,34 @@
+import { useGetIngredientsQuery } from '@/services/ingredient/api';
+import { Preloader } from '@krgaa/react-developer-burger-ui-components';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
+import { BurgerConstructor } from '@components/burger-constructor/burger-constructor';
+import { BurgerIngredients } from '@components/burger-ingredients/burger-ingredients';
+
+import styles from './Home.module.css';
+
+export const Home = (): React.JSX.Element => {
+  const { isLoading: loadingApp, error: errorApp } = useGetIngredientsQuery();
+
+  if (errorApp) return <h2>{'Ошибка'}</h2>;
+
+  return (
+    <>
+      {loadingApp && <Preloader />}
+
+      {!loadingApp && (
+        <>
+          <h1 className="text text_type_main-large mt-10 mb-5 pl-5">Соберите бургер</h1>
+
+          <div className={`${styles['constructor-wrapper']} pl-5 pr-5`}>
+            <DndProvider backend={HTML5Backend}>
+              <BurgerIngredients />
+              <BurgerConstructor />
+            </DndProvider>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
